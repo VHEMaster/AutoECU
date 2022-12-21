f7a23403e6618e59a9a9ffaa335e1bee2e5aad47
Maksym Naumchuk <vhemaster98@gmail.com>
2022-12-21 22:50:03 +0200

    Reset MCU if periph was not initialized

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1660,6 +1660,7 @@ void Error_Handler(void)
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1) {
+    NVIC_SystemReset();
   }
   /* USER CODE END Error_Handler_Debug */
 }
