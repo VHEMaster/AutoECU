ef2e1a5dd5d78a981085d2ee861d177e9dc651a1
Maksym Naumchuk <vhemaster98@gmail.com>
2023-02-22 13:19:40 +0200

    Enable AutoBaudRate feature

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1354,7 +1354,9 @@ static void MX_UART5_Init(void)
   huart5.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart5.Init.OverSampling = UART_OVERSAMPLING_16;
   huart5.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart5.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  huart5.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_AUTOBAUDRATE_INIT;
+  huart5.AdvancedInit.AutoBaudRateEnable = UART_ADVFEATURE_AUTOBAUDRATE_ENABLE;
+  huart5.AdvancedInit.AutoBaudRateMode = UART_ADVFEATURE_AUTOBAUDRATE_ON0X55FRAME;
   if (HAL_UART_Init(&huart5) != HAL_OK) {
     Error_Handler();
   }
