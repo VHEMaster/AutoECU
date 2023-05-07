8c5c407bb3e7e641cda3abfbed2e711242f035aa
Maksym Naumchuk <vhemaster98@gmail.com>
2023-05-07 23:13:08 +0300

    Update another config after adj on hw

--- Core/Src/config.c
+++ Core/Src/config.c
@@ -364,8 +364,8 @@ static const float default_start_ignition[TABLE_TEMPERATURES_MAX] = {
 };
 
 static const float default_start_idle_valve_pos[TABLE_TEMPERATURES_MAX] = {
-    100.0f, 100.0f, 100.0f, 97.0f, 90.0f, 80.0f, 70.0f, 63.0f,
-    58.0f, 54.0f, 52.0f, 50.0f, 50.0f, 52.0f, 55.0f, 60.0f,
+    100.0f, 100.0f, 100.0f, 97.0f, 70.0f, 55.0f, 45.0f, 40.0f,
+    35.0f, 32.0f, 30.0f, 30.0f, 30.0f, 30.0f, 30.0f, 30.0f,
 };
 
 static const float default_idle_wish_rotates[TABLE_TEMPERATURES_MAX] = {
