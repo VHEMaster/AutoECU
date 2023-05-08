fd983e2775db96db775dc6bc45aa9eabbefc9cf3
Maksym Naumchuk <vhemaster98@gmail.com>
2023-05-08 22:41:03 +0300

    Median of min/max values of enrichment load states

--- Math/math_misc.c
+++ Math/math_misc.c
@@ -29,3 +29,32 @@ INLINE void math_minmax(const float *array, unsigned int size, float *pmin, floa
     *pmin = min;
 
 }
+
+INLINE float math_median(const float *array, unsigned int size)
+{
+  float value = 0;
+  float sorted[size];
+  unsigned char changed = 0;
+
+  for(int i = 0; i < size; i++) {
+    sorted[i] = array[i];
+  }
+
+  for (int i = 0; i < size - 1; i++) {
+    changed = 0;
+    for (int j = 0; j < size - i - 1; j++) {
+      if (sorted[j] > sorted[j + 1]) {
+        value = sorted[j];
+        sorted[j] = sorted[j + 1];
+        sorted[j + 1] = value;
+        changed = 1;
+      }
+    }
+    if(!changed)
+      break;
+  }
+
+  value = sorted[size >> 1];
+
+  return value;
+}
